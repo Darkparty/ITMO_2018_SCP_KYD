5fd846264d2494012f8129e72fdbf23466437949
Yaroslav <34842216+kydwork@users.noreply.github.com>
2018-02-16 15:32:48 +0300

    init

---
+++ controller/App.controller.js
@@ -0,0 +1,49 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/unified/DateRange"
+], function (Controller, DateRange) {
+	"use strict";
+	return Controller.extend("sap.ui.MyCalendar.controller.App", {
+		onInit: function() {
+			var req = new XMLHttpRequest();
+			req.open('GET', '/resourse/calendar.xml', false); 
+			req.send(null);
+			var response;
+			if(req.status == 200) {
+  				response = req.responseText;
+			}
+
+			var oXMLModel =  new sap.ui.model.xml.XMLModel();
+			oXMLModel.setXML(response);
+			var oCalendar   = this.getView().byId("calendar");
+			var holidays = new Array();
+			for (var i = 0; i < 8; i++){
+				holidays[i+1] = oXMLModel.getProperty("/holidays/holiday/" + i + "/@title");
+			}
+			try{
+				for (var i = 0; i < 100; i++){
+					if (oXMLModel.getProperty("/days/day/" + i +"/@h") != null){
+						oCalendar.addSpecialDate(
+							new sap.ui.unified.DateTypeRange({
+  							startDate : new Date(2018, oXMLModel.getProperty("/days/day/" + i + "/@d").substring(0,2)-1,
+ 							oXMLModel.getProperty("/days/day/" + i +"/@d").substring(3 ,5)),
+ 							type   : sap.ui.unified.CalendarDayType.Type01,
+ 		 					tooltip   : holidays[oXMLModel.getProperty("/days/day/" + i +"/@h")]
+ 			 				})
+ 			 			);
+					} else {
+						if (oXMLModel.getProperty("/days/day/" + i + "/@t") == 1){
+						oCalendar.addSpecialDate(
+							new sap.ui.unified.DateTypeRange({
+  							startDate : new Date(2018, oXMLModel.getProperty("/days/day/" + i + "/@d").substring(0,2)-1,
+ 							oXMLModel.getProperty("/days/day/" + i +"/@d").substring(3 ,5)),
+ 							type   : sap.ui.unified.CalendarDayType.NonWorking
+ 		 					})
+ 			 			);
+						}
+					}		
+				}
+			} catch(e){}
+		}
+	});
+});
